eeba9be5a56272d57929cc21fb101382c490d6f4
Arcadie Cracan <acracan@gmail.com>
2018-12-29 19:46:16 +0200

    Make parse_tokens public

--- src/parser.h
+++ src/parser.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 #include "gcode_program.h"
 
 namespace gpr {
-
+    
+  block parse_tokens(const std::vector<string>& tokens);
   std::vector<std::string> lex_block(const std::string& block_text);
 
   gcode_program parse_gcode(const std::string& program_text);
